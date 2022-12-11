07efd3dbba70bf00d4efc22b30fd33b65f18b2d0
kazune-br <kazune.miyagi92@gmail.com>
2022-12-11 16:36:58 +0900

    feat: create api cleint to get todo list and todo item

---
+++ lib/infrastructure/api_client.dart
@@ -0,0 +1,20 @@
+// Package imports:
+import 'package:dio/dio.dart';
+import 'package:retrofit/retrofit.dart';
+
+// Project imports:
+import 'package:flutter_test_practice/infrastructure/dto.dart';
+
+part 'api_client.g.dart';
+
+@RestApi(
+    baseUrl: "https://jsonplaceholder.typicode.com/")
+abstract class ApiClient {
+  factory ApiClient(Dio dio, {String baseUrl}) = _ApiClient;
+
+  @GET("/todos")
+  Future<List<TodoDto>> getTodoList();
+
+  @GET("/todos/{id}")
+  Future<List<TodoDto>> getTodo(@Path("id") int id);
+}
---
+++ lib/infrastructure/dto.dart
@@ -0,0 +1,28 @@
+// Dart imports:
+import 'dart:convert';
+
+// Package imports:
+import 'package:json_annotation/json_annotation.dart';
+
+part 'dto.g.dart';
+
+@JsonSerializable(explicitToJson: true)
+class TodoDto {
+  @JsonKey(name: 'userId')
+  int? userId;
+  @JsonKey(name: 'id')
+  int? id;
+  @JsonKey(name: 'title')
+  String? title;
+  @JsonKey(name: 'completed')
+  bool? completed;
+
+  TodoDto({this.userId, this.id, this.title, this.completed});
+
+  factory TodoDto.fromJson(Map<String, dynamic> json) => _$TodoDtoFromJson(json);
+
+  Map<String, dynamic> toJson() => _$TodoDtoToJson(this);
+
+  @override
+  String toString() => json.encode(toJson());
+}
